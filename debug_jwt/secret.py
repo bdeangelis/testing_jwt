dfc7d177acbfb8bf2c43a9eee2b8e035621730ea
Brett DeAngelis <brett.deangelis@nutrien.com>
2021-08-09 11:26:21 -0400

    Possible conflicting comment

--- debug_jwt/secret.py
+++ debug_jwt/secret.py
@@ -51,3 +51,4 @@ def get_parameter(parameter_name, with_decryption):
         return {"error": ["Missing Token"]}
     return result["Parameter"]["Value"]
 
+# Here is another comment that comes from the new branch, will it conflict?
